d9752eec60368eac84ffe99e80958bc524b905f0
Beksultan <70627749+beksultan-07@users.noreply.github.com>
2022-03-01 13:09:17 +0600

    finished celenders list

--- src/pages/dividendCalendar/DividendC.tsx
+++ src/pages/dividendCalendar/DividendC.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import CelenderListD from '../../components/celenderComponents/celenderList/CelenderListD'
+import CelenderTop from '../../components/celenderComponents/celenderTop/CelenderTop'
+import { Container } from '../../uikit/uikit'
 
 const DividendC:React.FC = () => {
+  const [info, setInfo] = React.useState({
+    header: ['Symbol','Name','Ex-Dividend Date','Payment Date','Record Date','Dividend','Indicated Annual Dividend','Announcement Date'],
+    value: [
+      ['NXPI', 'HighPeak Energy, Inc.', '02/01/2022', '02/01/2022', '02/01/2022', '0.025', '0.1', '01/11/2022'],
+      ['NXPI', 'HighPeak Energy, Inc.', '02/01/2022', '02/01/2022', '02/01/2022', '0.025', '0.1', '01/11/2022'],
+      ['NXPI', 'HighPeak Energy, Inc.', '02/01/2022', '02/01/2022', '02/01/2022', '0.025', '0.1', '01/11/2022'],
+      ['NXPI', 'HighPeak Energy, Inc.', '02/01/2022', '02/01/2022', '02/01/2022', '0.025', '0.1', '01/11/2022'],
+      ['NXPI', 'HighPeak Energy, Inc.', '02/01/2022', '02/01/2022', '02/01/2022', '0.025', '0.1', '01/11/2022'],
+      ['NXPI', 'HighPeak Energy, Inc.', '02/01/2022', '02/01/2022', '02/01/2022', '0.025', '0.1', '01/11/2022'],
+
+    ]
+  })
+
   return (
-    <div>dividendC</div>
+    <Container>
+      <CelenderTop/>
+      <CelenderListD header={info.header} value={info.value}/>
+    </Container>
   )
 }
 
